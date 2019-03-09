af6ed9dce50e09e15316b6f4d926ec9921f4204d
Terence Liu <llebox@hotmail.com>
2019-03-09 04:48:29 -0500

    switched from mllib to ml, added ml pipeline, train/test separation based on grouped timestamp, gridsearch does not work yet

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from pyspark import SparkContext
 from pyspark.rdd import RDD
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import LinearRegression, LinearRegressionModel, LinearRegressionSummary, LinearRegressionTrainingSummary
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator, CrossValidatorModel
 from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD, RidgeRegressionWithSGD
 from pyspark.mllib import linalg as mllib_linalg
 from pyspark.ml import linalg as ml_linalg
@@ -49,33 +53,68 @@ def load_data(filename):
     spark.conf.set('spark.executor.cores', '50')
     spark.conf.set('spark.cores.max', '50')
     spark.conf.set("spark.driver.memory",'32g')
+    spark.sparkContext.setLogLevel("ERROR")
 
     # load df, choose a few features to start with without any null values
     # cast each column from string to float
-    df = spark.read.csv(filename, header=True).select(['id', 'timestamp', 'fundamental_2', 'fundamental_7', 'y'])
+    # need a specific set of methods to preprocess/fill/generate new features
+    # need to fill null values with avg/median, test difference in between
+    ind_vars = ['technical_20', 'fundamental_53', 'technical_30', 'technical_27', 'derived_0',\
+        'fundamental_42', 'fundamental_48', 'technical_21', 'technical_24', 'fundamental_11',\
+        'fundamental_44', 'technical_19', 'technical_13', 'technical_17', 'fundamental_9']
+    total_features = list(['id', 'timestamp', 'y'] + ind_vars)
+    df = spark.read.csv(filename, header=True).select(total_features)
     df = df.select(*(col(c).cast("float").alias(c) for c in df.columns)).na.fill(0)
-    num_id, num_timestamp = df.select("id").distinct().count(), df.select("timestamp").distinct().count()
-    market_df = df.select(["timestamp", 'fundamental_2', 'fundamental_7', "y"]).groupBy("timestamp").sort(df.timestamp)
-    print(market_df.show())
 
-    # set up features as one vector and output variable combined as labeledpoint
-    assembler = VectorAssembler(inputCols=['fundamental_2', 'fundamental_7'], outputCol="features")
-    transformed = assembler.transform(df).select(col("y").alias("label"), col("features"))
-    labeledRDD = transformed.rdd.map(lambda row:LabeledPoint(row.label, as_old(row.features)))
+    # separate training/testing based on 80/20
+    timestamps = df.select("timestamp").distinct().sort(df.timestamp).rdd.flatMap(lambda x:x).collect()
+    train_len = int(len(timestamps)*0.8)
+    train_df, test_df = df.filter(df.timestamp.isin(list(timestamps)[:train_len])), df.filter(df.timestamp.isin(list(timestamps)[train_len:]))
+
+    # set up ML pipeline
+    assembler = VectorAssembler(inputCols=ind_vars, outputCol='features')
+    lr = LinearRegression(featuresCol='features', labelCol='y', maxIter=10, regParam=0.00001)
+    pipeline = Pipeline(stages=[assembler, lr])
+    train_transformed = assembler.transform(train_df).select(['features','y'])
+    test_transformed = assembler.transform(test_df).select(['features','y'])
+
+    # train/testing using linear model
+    lr_model = lr.fit(train_transformed)
+    trainingSummary = lr_model.summary
+    print("RMSE: ", trainingSummary.rootMeanSquaredError)
+    print("R^2: ", trainingSummary.r2)
+    lr_predictions = lr_model.transform(test_transformed)
+    print(lr_predictions.show())
+    lr_evaluator = RegressionEvaluator(predictionCol="prediction", labelCol="y", metricName="rmse")
+    rmse = lr_evaluator.evaluate(lr_predictions)
+    print("RMSE: ", rmse)
+    r2 = lr_evaluator.evaluate(lr_predictions, {lr_evaluator.metricName: "r2"})
+    print("r2: ", r2)
+
+    # grid search not working...
+    grid_lr = ParamGridBuilder().addGrid(lr.regParam, [0.01, 1.0]).build()
+    cv = CrossValidator(estimator=pipeline, estimatorParamMaps=grid_lr, evaluator=lr_evaluator)
+    cv_model = cv.fit(train_df)
+    print(cv_model.bestModel.extractParamMap())
+    '''
+    train_transformed = assembler.transform(train_df).select(col("y"), col("features"))
+    test_transformed = assembler.transform(test_df).select(col("y"), col("features"))
+    train_labeledRDD = train_transformed.rdd.map(lambda row:LabeledPoint(row.label, as_old(row.features)))
+    test_labeledRDD = test_transformed.rdd.map(lambda row:LabeledPoint(row.label, as_old(row.features)))
   
     # train the linear model with stochatic gradient descent
-    linear_model = LinearRegressionWithSGD.train(labeledRDD, iterations=100, step=0.0000001)
-    linear_preds = labeledRDD.map(lambda p: (p.label, linear_model.predict(p.features)))
+    linear_model = LinearRegressionWithSGD.train(train_labeledRDD, iterations=100, step=0.0000001)
+    linear_preds = train_labeledRDD.map(lambda p: (p.y, linear_model.predict(p.features)))
     linear_MSE = linear_preds.map(lambda x:(x[0] - x[1])**2).reduce(lambda x,y:x + y) / linear_preds.count()
     print("Linear Regression Mean Squared Error = " + str(linear_MSE))
-
+    
     # train the ridge model with stochastic gradient descent
     ridge_model = RidgeRegressionWithSGD.train(labeledRDD, iterations=100, step=0.0000001)
-    ridge_preds = labeledRDD.map(lambda p: (p.label, ridge_model.predict(p.features)))
+    ridge_preds = labeledRDD.map(lambda p: (p.y, ridge_model.predict(p.features)))
     ridge_MSE = ridge_preds.map(lambda x:(x[0] - x[1])**2).reduce(lambda x,y:x + y) / ridge_preds.count()
     print("Ridge Regression Mean Squared Error = " + str(ridge_MSE))
     # split: train = df.limit(timestamp_used_for_training), test = df.subtract(train)
-
+    '''
 
 if __name__ == "__main__":
     '''
